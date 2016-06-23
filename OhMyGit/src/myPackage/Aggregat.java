c673e779ac67c9ca837f2953f492e40b16ce23e9
ctrommer <ctrommer@mail.de>
2016-06-23 08:13:07 +0200

    Aggregat verändert, Material neu

--- OhMyGit/src/myPackage/Aggregat.java
+++ OhMyGit/src/myPackage/Aggregat.java
@@ -5,6 +5,8 @@ public class Aggregat {
 	public static void main(String[] args) {
 		System.out.println("Hallo");
 		System.out.println("zweites Commit");
+		System.out.println("drittes Commit");
+		
 
 	}
 
